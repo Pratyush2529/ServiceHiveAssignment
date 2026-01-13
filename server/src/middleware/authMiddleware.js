4d33f1406c39097392f0c42841c83bfc87fceee6
Pratyush Sharma <pratyushsharma25feb@gmail.com>
2026-01-13 18:15:46 +0530

    minor fix

--- server/src/middleware/authMiddleware.js
+++ server/src/middleware/authMiddleware.js
@@ -5,12 +5,8 @@ const User = require('../models/User');
 exports.protect = async (req, res, next) => {
     try {
 
-        let token;
-        if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-            token = req.headers.authorization.split(' ')[1];
-        } else if (req.cookies.jwt) {
-            token = req.cookies.jwt;
-        }
+
+        const token = req.cookies.jwt;
 
         if (!token) {
             return res.status(401).json({ message: 'You are not logged in! Please log in to get access.' });
