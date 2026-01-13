a68941434bdf477f5457bd945072d57a43f4e163
Pratyush Sharma <pratyushsharma25feb@gmail.com>
2026-01-13 18:26:53 +0530

    jwt bug fix

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -13,7 +13,8 @@ const sendToken = (user, statusCode, res) => {
     const cookieOptions = {
         expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production'
+        sameSite: "none",
+        secure:true
     };
 
     res.cookie('jwt', token, cookieOptions);
@@ -73,10 +74,7 @@ exports.login = async (req, res, next) => {
 };
 
 exports.logout = (req, res) => {
-    res.cookie('jwt', 'loggedout', {
-        expires: new Date(Date.now() + 10 * 1000),
-        httpOnly: true
-    });
+    res.clearCookie("jwt", cookieOptions);
     res.status(200).json({ status: 'success' });
 };
 
